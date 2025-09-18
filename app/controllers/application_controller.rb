7b9af006400b22170273ce559e1fbef4f173503e
umar.ramadhana <umar.ramadhana@everywhere.id>
2025-09-18 22:32:30 +0700

    adding API error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::API
+  rescue_from StandardError,
+              with: :handle_error
+
   private
+
+  def handle_error(e)
+    error_message = e&.message.to_s
+    status = 500
+
+    case error_message
+    when ConstErr::UNAUTHORIZED
+      status = 401
+    when ConstErr::MUST_WAKE_FIRST
+      status = 400
+    end
+
+    render_response(status: status, error: error_message)
+  end
 
   def render_response(status: 200, data: {}, error: "")
     formatted_data = { data: data, error: error }
