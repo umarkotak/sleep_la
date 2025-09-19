a68b832af2b35fe69cf3420561290ba131530bf5
umar.ramadhana <umar.ramadhana@everywhere.id>
2025-09-20 06:28:24 +0700

    update route naming with app name and version for prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root "health#health_check"
-  get "/health" => "health#health_check"
+  get "/goodnight/api/health" => "health#health_check"
 
-  post "/sleep/start" => "sleep_logs#start_sleep"
-  post "/sleep/finish" => "sleep_logs#finish_sleep"
-  get "/sleep_logs/me" => "sleep_logs#get_my_sleep_logs"
-  get "/sleep_logs/friends" => "sleep_logs#get_friends_sleep_logs"
-  post "/user/:user_guid/follow" => "users#follow"
-  post "/user/:user_guid/unfollow" => "users#unfollow"
+  post "/goodnight/api/v1/sleep/start" => "sleep_logs#start_sleep"
+  post "/goodnight/api/v1/sleep/finish" => "sleep_logs#finish_sleep"
+  get "/goodnight/api/v1/sleep_logs/me" => "sleep_logs#get_my_sleep_logs"
+  get "/goodnight/api/v1/sleep_logs/friends" => "sleep_logs#get_friends_sleep_logs"
+  post "/goodnight/api/v1/user/:user_guid/follow" => "users#follow"
+  post "/goodnight/api/v1/user/:user_guid/unfollow" => "users#unfollow"
 
   match "*path", to: "errors#route_not_found", via: :all
 end
